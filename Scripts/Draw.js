9693dfe458dd9086d44d2d0831f9517825ec88cb
Turtle <125102455+Programer-Turtle@users.noreply.github.com>
2024-11-02 16:23:19 -0400

    Add files via upload

---
+++ Scripts/Draw.js
@@ -0,0 +1,8 @@
+let WidthResIn = document.getElementById("WidthRes")
+let HeightResIn = document.getElementById("HeightRes")
+let ColorIn = document.getElementById("Color")
+let Canvas = document.getElementById("Screen")
+let Context = Canvas.getContext('2d')
+
+WidthResIn.value = 1
+HeightResIn.value = 1
